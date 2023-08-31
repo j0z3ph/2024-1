94d9426409a826015abe5f5546d5a3614169a3b0
j0z3ph <j0z3ph@gmail.com>
2023-08-30 20:34:59 -0600

    Mi primer programa

---
+++ IP/C1/hola.c
@@ -0,0 +1,16 @@
+/**
+ * @file hola.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa
+ * @version 0.1
+ * @date 2023-08-30
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include<stdio.h>
+int main()
+{
+    printf("Hola Mundillo");
+    return 0;
+}
