6058d11f7ba2cd202e925dc1ac117f010d0caf40
j0z3ph <j0z3ph@gmail.com>
2023-09-04 19:32:23 -0600

    Segundo programa

---
+++ IP/C2/mi_programa.c
@@ -0,0 +1,17 @@
+/**
+ * @file mi_programa.c
+ * @author Jose Luis (jlcruz@ipn.mx)
+ * @brief Mi segundo programa
+ * @version 0.1
+ * @date 2023-09-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    printf("Holi Canoli");
+    return 0;
+}
