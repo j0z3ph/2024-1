c8fb4623114fbd97394104ea64635433423b5a24
Jose Luis Cruz <j0z3ph@gmail.com>
2023-10-04 14:27:18 -0600

    Funciones

---
+++ HC/C4/maizoro.m
@@ -0,0 +1,15 @@
+function [sum, res, mul, div] = maizoro(valor1, valor2)
+%MAIZORO Hace las 4 operaciones basicas
+% Entradas
+%   -valor1
+%   -valor2
+% Salidas
+%   - Suma
+%   - Resta
+%   - Multiplicacion
+%   - Division
+sum = valor1 + valor2;
+res = valor1 - valor2;
+mul = valor1 * valor2;
+div = valor1 / valor2;
+end
---
+++ HC/C4/suma.m
@@ -0,0 +1,10 @@
+function resultado = suma(valor1, valor2)
+%SUMA Realiza la suma de dos numeros
+% Entradas
+%   - valor1 Numero
+%   - valor2 Numero
+% Salidas
+%   - resultado valor1 + valor2
+
+resultado = valor1 + valor2;
+end
