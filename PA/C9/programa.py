69ec3b4a7822f83a4116e2dd9d69ff6941d983c8
Jose Luis Cruz <j0z3ph@gmail.com>
2023-10-10 15:01:10 -0600

    Plantilla

---
+++ PA/C9/programa.py
@@ -0,0 +1,15 @@
+from Ui_mi_ventana import *
+from PyQt6.QtWidgets import QMainWindow
+import sys
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *parent, **flags) -> None:
+        super().__init__(*parent, **flags)
+        self.setupUi(self)
+        
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+    
