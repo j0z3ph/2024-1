2e051bb57bf732919285625e432e43bd03cdff5d
j0z3ph <j0z3ph@gmail.com>
2023-10-10 08:55:41 -0600

    Ejemplo

--- PA/C8/ventana.py
+++ PA/C8/ventana.py
@@ -1,12 +1,27 @@
-import tkinter as tk
-import tkinter.messagebox as msg
+from PyQt6.QtWidgets import QMainWindow, QMessageBox
+from Ui_ventana import *
+import sys
 
-def funcion():
-    msg.showinfo("Titulo", "Esto es un mensaje")
-
-ventana = tk.Tk()
-ventana.wm_title("Hola")
-ventana.wm_minsize(300,300)
-tk.Button(master=ventana, text="Pushame", command=funcion).pack()
-
-ventana.mainloop()
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *parent, **flags) -> None:
+        super().__init__(*parent, **flags)
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.showMessage)
+    def showMessage(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Icon.Information)
+        msg.setText("This is a message box")
+        msg.setInformativeText("This is additional information")
+        msg.setWindowTitle("MessageBox demo")
+        msg.setDetailedText("The details are as follows:")
+        msg.setStandardButtons(QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel)
+        retval = msg.exec()
+        print("value of pressed message box button:", retval)
+    
+        
+        
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
